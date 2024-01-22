a01e6f2774e105bd1be5659eebc92136d534142b
Nahum_Zewdie_GitHub <97863835+nzewdie-UCCS-Student@users.noreply.github.com>
2024-01-21 20:17:53 -0700

    Ensured camera's transform position follows player's transform position

--- The Cannonball That Could/Assets/Scripts/CameraFollower.cs
+++ The Cannonball That Could/Assets/Scripts/CameraFollower.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class CameraFollower : MonoBehaviour
 {
+    // movement fields
+    public float cameraSpeed = 3.0f;
+    Transform target;
     // Start is called before the first frame update
     void Start()
     {
