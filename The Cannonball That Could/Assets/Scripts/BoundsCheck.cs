a07707c631765aae6ca3003aceee75f090223bb6
Nahum_Zewdie_GitHub <97863835+nzewdie-UCCS-Student@users.noreply.github.com>
2024-01-24 12:07:16 -0700

    Changed colliders for player (Circle collider)

--- The Cannonball That Could/Assets/Scripts/BoundsCheck.cs
+++ The Cannonball That Could/Assets/Scripts/BoundsCheck.cs
@@ -2,19 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 /// <summary>
-/// Used for camera clamping
+/// Used to keep gameobject on screen
 /// </summary>
 public class BoundsCheck : MonoBehaviour
 {
+    // Collider fields
+    CircleCollider2D collider;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        collider = GetComponent<CircleCollider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+    private
 }
