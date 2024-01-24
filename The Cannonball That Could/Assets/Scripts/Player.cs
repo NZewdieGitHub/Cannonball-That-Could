3447a59f907beb940f89bc8ee0945dd0a60acc50
Nahum_Zewdie_GitHub <97863835+nzewdie-UCCS-Student@users.noreply.github.com>
2024-01-24 09:55:08 -0700

    Tested and completed restart functionality

--- The Cannonball That Could/Assets/Scripts/Player.cs
+++ The Cannonball That Could/Assets/Scripts/Player.cs
@@ -1,14 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class Player : MonoBehaviour
 {
     // movement fields
     public Rigidbody2D rb2d;
     public float accelleration = 4f;
     Vector2 movement;
-
+    public string levelName;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,6 +23,11 @@ public class Player : MonoBehaviour
         movement.x = Input.GetAxis("Horizontal");
         movement.y = Input.GetAxis("Vertical");
 
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            RestartLevel();
+        }
+            
     }
     /// <summary>
     /// Make player move automatically
@@ -31,5 +36,10 @@ public class Player : MonoBehaviour
     {
         // move cannon ball freely
         rb2d.MovePosition(rb2d.position + movement * accelleration * Time.deltaTime);
+    }
+
+    public void RestartLevel()
+    {
+        SceneManager.LoadScene(levelName);
     }
 }
