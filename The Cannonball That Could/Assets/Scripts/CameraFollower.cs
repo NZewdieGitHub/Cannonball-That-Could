085bdd140ee6134126686c52a12a4fa876c13f4a
Nahum_Zewdie_GitHub <97863835+nzewdie-UCCS-Student@users.noreply.github.com>
2024-01-25 11:50:02 -0700

    Added serialized fields to hold text as a timer and as a game object

--- The Cannonball That Could/Assets/Scripts/CameraFollower.cs
+++ The Cannonball That Could/Assets/Scripts/CameraFollower.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -11,10 +12,15 @@ public class CameraFollower : MonoBehaviour
     [SerializeField]
     Player player;
 
+    // Text Field
+    [SerializeField]
+    TextMeshProUGUI TimeText;
+    public float exitTime = 3f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Setup initial text
+        TimeText.SetText("Get back in: " + exitTime.ToString());
     }
 
     // Update is called once per frame
