e152a087a3ac3eb9e3bbe4d822e1ae211dd1adf6
Nahum_Zewdie_GitHub <97863835+nzewdie-UCCS-Student@users.noreply.github.com>
2024-01-29 08:18:57 -0700

    Created a firing range for the canon ball to reside in before firing

--- The Cannonball That Could/Assets/Scripts/Enemy.cs
+++ The Cannonball That Could/Assets/Scripts/Enemy.cs
@@ -1,21 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-using TMPro;
 /// <summary>
 /// Used for The enemy ship
 /// </summary>
 public class Enemy : MonoBehaviour
 {
     // Collider field
-    public BoxCollider2D enemyCollider;
-
-    // Text Updating fields
-    [SerializeField]
-    public TextMeshProUGUI PlayerUI;
-    public int playerScore = 3;
-
+    BoxCollider2D collider;
     // Start is called before the first frame update
     void Start()
     {
@@ -26,26 +18,5 @@ public class Enemy : MonoBehaviour
     void Update()
     {
         
-    }
-
-    /// <summary>
-    /// Used for colliding with the enemy ship
-    /// </summary>
-    /// <param name="collision"></param>
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-
-        // if player collides with enemy
-        if (collision.gameObject.CompareTag("Enemy"))
-        {
-            // updated score
-            playerScore -= 1;
-            PlayerUI.SetText("Player Health: " + playerScore.ToString());
-
-            // reposition player
-            transform.position = cannon.transform.position;
-            canFire = true;
-            vc.transform.position = new Vector3(-8.4989f, 0.37f, -21.64309f);
-        }
     }
 }
