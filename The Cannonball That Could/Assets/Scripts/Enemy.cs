1c6568cc69e111f7d94ad0793a21e49332e7884e
Nahum_Zewdie_GitHub <nzewdie@uccs.edu>
2024-01-28 22:08:51 -0700

    Created an enemy script

---
+++ The Cannonball That Could/Assets/Scripts/Enemy.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// Used for The enemy ship
+/// </summary>
+public class Enemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
