d7a4d46400c97426be7c55c6c78e0db877043bd5
Nahum_Zewdie_GitHub <97863835+nzewdie-UCCS-Student@users.noreply.github.com>
2024-02-06 13:04:31 -0700

    Created script for cannon to fire more cannonballs.

---
+++ The Cannonball That Could/Assets/Scripts/Cannon.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cannon : MonoBehaviour
+{
+    // Player Fields
+    [SerializeField]
+    GameObject CannonballPrefab;
+    [SerializeField]
+    Player playerScript;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
