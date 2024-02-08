90dcde0bafc922201887ae3acbca937e548389be
Nahum_Zewdie_GitHub <97863835+nzewdie-UCCS-Student@users.noreply.github.com>
2024-02-08 11:51:03 -0700

    Created an event manager script

--- The Cannonball That Could/Assets/Scripts/EventManager.cs
+++ The Cannonball That Could/Assets/Scripts/EventManager.cs
@@ -1,10 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-/// <summary>
-/// Handle Events
-/// </summary>
-public class EventHandler : MonoBehaviour
+
+public class EventManager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
