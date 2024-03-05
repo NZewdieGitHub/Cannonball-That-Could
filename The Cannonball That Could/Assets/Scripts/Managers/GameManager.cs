01b38e0579f3f7c50cb73121d66eb8d96e4c0270
Nahum_Zewdie_GitHub <97863835+nzewdie-UCCS-Student@users.noreply.github.com>
2024-03-05 10:04:48 -0700

    Successfully reset the cannonball's speed after it dies while boosted.

--- The Cannonball That Could/Assets/Scripts/Managers/GameManager.cs
+++ The Cannonball That Could/Assets/Scripts/Managers/GameManager.cs
@@ -46,8 +46,12 @@ public class GameManager : MonoBehaviour
         player.canFire = true;
         player.hasFired = false;
         vc.transform.position = new Vector3(5.71467f, 0.37f, -21.64309f);
-        // deactivate speed boost
-        player.isBoosted = false;
+        // respet player speed
+        if (player.isBoosted == true)
+        {
+            player.accelleration = (player.accelleration / 2);
+            player.isBoosted = false;
+        }
     }
     /// <summary>
     /// Pauses game
