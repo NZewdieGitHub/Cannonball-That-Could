eb8ecb34617cee443c2f4ec4335fde10a40273ad
Nahum_Zewdie_GitHub <97863835+nzewdie-UCCS-Student@users.noreply.github.com>
2024-03-21 10:44:52 -0600

    Adjusted enemy cannon ball prefab

--- The Cannonball That Could/Assets/Scripts/EnemyCannon.cs
+++ The Cannonball That Could/Assets/Scripts/EnemyCannon.cs
@@ -8,11 +8,23 @@ public class EnemyCannon : MonoBehaviour
     // fire point field
     public Transform firePoint;
 
+    // Enemy Prefab Sprite
+    public GameObject enemyBullet;
+    // Player field 
+    public Player player;
     // Update is called once per frame
     void Update()
     {
-        
+        // fire only when the player fires
+        if (player.cannonShot == true)
+        {
+            ShootEnemy();
+        }
     }
 
+    public void ShootEnemy()
+    {
+        // shooting logic
 
+    }
 }
