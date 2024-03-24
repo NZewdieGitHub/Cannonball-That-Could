0c747a94b24ffe4793012362053fb4e5984e2a20
Nahum_Zewdie_GitHub <97863835+nzewdie-UCCS-Student@users.noreply.github.com>
2024-03-23 21:45:28 -0600

    Added an event for the player firing the event

---
+++ The Cannonball That Could/Assets/Scripts/Events/PlayerCannonFiredEvent.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+/// <summary>
+/// A player cannon fired event
+/// </summary>
+public class PlayerCannonFiredEvent : UnityEvent
+{
+}
