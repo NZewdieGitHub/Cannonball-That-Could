238f3f55faf4a07e269b8bcea39b7ef2d991e197
Nahum_Zewdie_GitHub <nzewdie@uccs.edu>
2024-04-11 11:23:39 -0600

    Made sure the camera respawns where the player cannon is after a turn.

--- The Cannonball That Could/Assets/Scripts/Managers/GameManager.cs
+++ The Cannonball That Could/Assets/Scripts/Managers/GameManager.cs
@@ -63,7 +63,7 @@ public class GameManager : MonoBehaviour
         player.transform.position = cannon.transform.position;
         player.canFire = true;
         player.hasFired = false;
-        vc.transform.position = new Vector3(5.71467f, 0.37f, -21.64309f);
+        vc.transform.position = new Vector3(cannon.transform.position.x, 0.37f, -21.64309f);
         // respet player speed
         if (player.isBoosted == true)
         {
